c66f13984d0f542daa4246b759f67160580b023f
williamchavula <rumbani@gmail.com>
2025-05-09 16:18:32 -0500

    setup authentication with clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import type { Metadata } from "next";
+import {ClerkProvider} from "@clerk/nextjs";
+
 import { Inter } from "next/font/google";
 
 import "./globals.css";
@@ -21,8 +23,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+
+    <ClerkProvider>
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
+      </ClerkProvider>
   );
 }
