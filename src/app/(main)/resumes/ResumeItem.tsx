daae09ecf620011af6d51dd51c09231d9802fcfa
williamchavula <rumbani@gmail.com>
2025-05-20 08:26:01 -0500

    implement resume loading and preview on resumes page

---
+++ src/app/(main)/resumes/ResumeItem.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { ResumeServerData } from "@/lib/types";
+import Link from "next/link";
+import { formatDate } from "date-fns";
+import ResumePreview from "@/components/ResumePreview";
+import { toResumeFormValuesMapper } from "@/lib/utils";
+
+interface ResumeItemProps {
+  resume: ResumeServerData;
+}
+
+function ResumeItem({ resume }: ResumeItemProps) {
+  const wasUpdated = resume.updatedAt !== resume.createdAt;
+
+  return (
+    <div className="group hover:border-border bg-secondary rounded-lg border border-transparent p-3 transition-colors">
+      <div className="space-y-5">
+        <Link
+          href={`/editor?resumeId=${resume.id}`}
+          className="inline-block w-full text-center"
+        >
+          <p className="line-clamp-1 font-semibold">
+            {resume.title || "No title"}
+          </p>
+          {resume.description && (
+            <p className="line-clamp-2 text-sm">{resume.description}</p>
+          )}
+          <p className="text-muted-foreground text-xs">
+            {wasUpdated ? "Updated" : "Created"} on{" "}
+            {formatDate(resume.updatedAt, "MMM d, yyyy h:mm a")}
+          </p>
+        </Link>
+        <Link
+          href={`/editor?resumeId=${resume.id}`}
+          className="relative inline-block w-full"
+        >
+          <ResumePreview
+            resume={toResumeFormValuesMapper(resume)}
+            className="overflow-hidden shadow-sm transition-shadow group-hover:shadow-lg"
+          />
+          <div className="absolute inset-x-0 bottom-0 h-24 bg-gradient-to-t from-white to-transparent" />
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default ResumeItem;
--- src/app/(main)/resumes/page.tsx
+++ src/app/(main)/resumes/page.tsx
@@ -2,12 +2,37 @@ import { Metadata } from "next";
 import { Button } from "@/components/ui/button";
 import { PlusSquare } from "lucide-react";
 import Link from "next/link";
+import { auth } from "@clerk/nextjs/server";
+import prisma from "@/lib/prisma";
+import { resumeWithInclude } from "@/lib/types";
+import ResumeItem from "@/app/(main)/resumes/ResumeItem";
 
 export const metadata: Metadata = {
   title: "Your resumes",
 };
 
-function Page() {
+async function Page() {
+  const { userId, redirectToSignIn } = await auth();
+
+  if (!userId) {
+    return redirectToSignIn();
+  }
+
+  const [resumes, totalCount] = await Promise.all([
+    prisma.resume.findMany({
+      where: {
+        userId,
+      },
+      orderBy: {
+        updatedAt: "desc",
+      },
+      include: resumeWithInclude,
+    }),
+    prisma.resume.count({ where: { userId } }),
+  ]);
+
+  // Todo: Check quota for non-premium users
+
   return (
     <main className="mx-auto w-full max-w-7xl space-y-6 px-3 py-6">
       <Button asChild className="mx-auto flex w-fit gap-2">
@@ -16,6 +41,15 @@ function Page() {
           New resume
         </Link>
       </Button>
+      <div className="space-y-1">
+        <h1 className="text-3xl font-bold">Your resumes</h1>
+        <p>Total: {totalCount}</p>
+      </div>
+      <div className="flex w-full grid-cols-2 flex-col gap-3 sm:grid md:grid-cols-3 lg:grid-cols-4">
+        {resumes.map((resume) => (
+          <ResumeItem resume={resume} key={resume.id} />
+        ))}
+      </div>
     </main>
   );
 }
